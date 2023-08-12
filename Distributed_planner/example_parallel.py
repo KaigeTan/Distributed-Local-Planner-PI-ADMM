2b36d88228920916e72fe7d0b3b18fe67549926e
Kaige Tan <kaiget@UG.KTH.SE>
2023-08-12 10:03:59 +0800

    centralized planner done

---
+++ Distributed_planner/example_parallel.py
@@ -0,0 +1,92 @@
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+from pypoman import plot_polygon
+from cProfile import label
+import sys
+sys.path.append('../')
+import pyobca
+
+
+state_record = []
+# %% obca optimization
+optimizer = pyobca.OBCAOptimizer()
+init_state = [arr[0, :] for arr in optimizer.ref_traj]
+init_state = np.vstack(init_state)
+state_record = [init_state]
+init_state = np.reshape(init_state, [optimizer.num_veh*optimizer.n_states, 1]) # 10 X 1
+for t_step in range(int(optimizer.T/optimizer.dt - optimizer.N_horz)): # TODO: check if -1
+    if t_step == 19:
+        print('')
+    optimizer.initialize(t_step, init_state, max_x=150, max_y=20)
+    optimizer.build_model() # TODO: check if this step needs to be in the loop
+    optimizer.generate_constrain()
+    optimizer.generate_variable()
+        
+    r = 0.1*np.eye(optimizer.n_controls*optimizer.num_veh)
+    q = np.eye(optimizer.n_states*optimizer.num_veh)
+    optimizer.generate_object(r, q)
+    optimizer.solve()
+        
+    # check the optimization results
+    x_opt = np.array(np.round(optimizer.x_opt, decimals=3))
+    y_opt = np.array(np.round(optimizer.y_opt, decimals=3))
+    v_opt = np.array(np.round(optimizer.v_opt, decimals=3))
+    heading_opt = np.array(np.round(optimizer.theta_opt, decimals=3))
+    steer_opt = np.array(np.round(optimizer.steer_opt, decimals=3))
+    a_opt = np.array(np.round(optimizer.a_opt, decimals=3))
+    steerate_opt = np.array(np.round(optimizer.steerate_opt, decimals=3))
+        
+    # iterate to the next time step
+    init_state = np.concatenate((x_opt[:, 1].reshape((-1, 1)), 
+                                 y_opt[:, 1].reshape((-1, 1)), 
+                                 v_opt[:, 1].reshape((-1, 1)),
+                                 heading_opt[:, 1].reshape((-1, 1)),
+                                 steer_opt[:, 1].reshape((-1, 1))), axis=1)
+    state_record += [init_state]
+    init_state = np.reshape(init_state, [optimizer.num_veh*optimizer.n_states, 1]) # 10 X 1
+    
+# visualization
+fig, ax = plt.subplots()
+
+# plot the vehicle center trajectories
+v1x = [vec[0, 0] for vec in state_record]
+v1y = [vec[0, 1] for vec in state_record]
+v2x = [vec[1, 0] for vec in state_record]
+v2y = [vec[1, 1] for vec in state_record]
+ax.plot(v1x, v1y, 'go', ms=3, label='vehicle1 path')
+ax.plot(v2x, v2y, 'ro', ms=3, label='vehicle2 path')
+ax.set_xlim(0,90)
+
+# plot the vehicle polygons
+for i in range(len(state_record)):
+    v1_verts = pyobca.generate_vehicle_vertices(state_record[i][0, [0, 1, 3]])
+    plot_polygon(v1_verts, fill=False, color='b')
+    v2_verts = pyobca.generate_vehicle_vertices(state_record[i][1, [0, 1, 3]])
+    plot_polygon(v2_verts, fill=False, color='r')
+
+plt.show()
+
+# plot_polygon(parking_obs1)
+# plot_polygon(parking_obs2, color='r')
+# ax.legend()
+# result_state = np.array([x_opt, y_opt, heading_opt]).T
+# for state in result_state:
+#     verts = pyobca.generate_vehicle_vertices(
+#         pyobca.SE2State(state[0], state[1], state[2]), base_link=True)
+#     plot_polygon(verts, fill=False, color='b')
+# v_opt = optimizer.v_opt.elements()
+# a_opt = optimizer.a_opt.elements()
+# t = [optimizer.T*k for k in range(len(v_opt))]
+# t_a = [optimizer.T*k for k in range(len(a_opt))]
+
+
+# fig2, ax2 = plt.subplots(3)
+# ax2[0].plot(t, v_opt, label='v-t')
+# ax2[1].plot(t_a, a_opt, label='a-t')
+# ax2[2].plot(t, steer_opt, label='steering-t')
+# ax2[0].legend()
+# ax2[1].legend()
+# ax2[2].legend()
+# plt.show()
+
---
+++ Distributed_planner/pyobca/optimizer.py
@@ -0,0 +1,187 @@
+from .search import VehicleConfig
+import casadi as ca
+import numpy as np
+from math import *
+from .util import compute_square_halfspaces_ca, generate_vehicle_vertices
+
+class OBCAOptimizer:
+    def __init__(self, cfg: VehicleConfig = VehicleConfig()) -> None:
+        self.L = cfg.length
+        self.offset = cfg.length/2 - cfg.baselink_to_rear
+        self.lf = cfg.lf
+        self.lr = cfg.lr
+        self.v_cfg = cfg
+        self.n_controls = 2
+        self.n_states = 5 # x, y, v, heading angle, steer angle
+        self.n_dual_variable = 4 # dual variable dimension is same to the vertices
+        self.num_veh = 2
+        self.obstacles = []
+        self.G = ca.DM([[1, 0],
+                     [-1, 0],
+                     [0, 1],
+                     [0, -1], ])
+        self.g = ca.vertcat(ca.SX([cfg.length/2, cfg.length/2,
+                                   0.5*cfg.width, 0.5*cfg.width]))
+        self.T = cfg.T
+        self.dt = cfg.dt
+        self.ref_traj = cfg.ref_traj_gen()
+        self.N_horz = 10 # control horizon
+
+    def initialize(self, t_step, init_state, max_x, max_y):
+        
+        self.constrains = []
+        self.x0 = []
+        self.lbg = []
+        self.ubg = []
+        self.lbx = []
+        self.ubx = []
+        self.variable = []
+        self.init_state = ca.SX(init_state) # the current state at each time step, numerical value, num_veh X n_state
+        
+        for i_t in range(self.N_horz):
+            x0_temp = []
+            for i_veh in range(self.num_veh):
+                x0_temp = np.hstack([x0_temp, self.ref_traj[i_veh][t_step+i_t]])
+            self.x0 += [x0_temp] # state variable initialization, N_horz X num_veh*n_state
+        self.x0 += [[0]*(self.num_veh*self.n_controls*(self.N_horz-1))] # control variable initialization
+        self.x0 += [[0.1]*(self.num_veh*self.n_dual_variable*self.N_horz)] # TODO: check dual variable number, dual variable initialization
+        self.max_x = max_x
+        self.max_y = max_y
+
+    def build_model(self) -> bool:
+        
+        x = ca.SX.sym('x', self.num_veh)
+        y = ca.SX.sym('y', self.num_veh)
+        v = ca.SX.sym('v', self.num_veh)
+        theta = ca.SX.sym('theta', self.num_veh)
+        steering = ca.SX.sym('steering', self.num_veh)
+        a = ca.SX.sym('a', self.num_veh)
+        steering_rate = ca.SX.sym('steering_rate', self.num_veh)
+        self.state = ca.vertcat(ca.vertcat(x, y, v, theta), steering) # 5*num_veh X 1
+        self.control = ca.vertcat(a, steering_rate) # 2*num_veh X 1
+        beta = ca.atan(self.lr*ca.tan(steering)/(self.lr+self.lf))
+        self.rhs = ca.vertcat(ca.vertcat(v*ca.cos(theta+beta), v*ca.sin(theta+beta),
+                                   a, v/self.lr*ca.sin(beta)), steering_rate) # 5*num_veh X 1, TODO: doublecheck the last-but-two term
+        self.f = ca.Function('f', [self.state, self.control], [self.rhs])
+        self.X = ca.SX.sym('X', self.num_veh, self.n_states, self.N_horz) # state and ctrl variable indicates the global optimization
+        self.U = ca.SX.sym('U', self.num_veh, self.n_controls, self.N_horz-1)
+        self.LAMBDA = ca.SX.sym('LAMBDA', self.num_veh, self.n_dual_variable, self.N_horz)
+        self.obj = 0
+
+        return True
+
+    def solve(self):
+        nlp_prob = {'f': self.obj, 'x': ca.vertcat(*self.variable),
+                    'g': ca.vertcat(*self.constrains)}
+        solver = ca.nlpsol('solver', 'ipopt', nlp_prob)
+        sol = solver(x0=ca.vertcat(*self.x0), lbx=self.lbx, ubx=self.ubx,
+                     ubg=self.ubg, lbg=self.lbg)
+        u_opt = sol['x']
+        # get optimized result
+        state_num = self.num_veh*self.n_states*self.N_horz
+        action_num = self.num_veh*self.n_controls*(self.N_horz-1)
+        self.x_opt = ca.reshape(u_opt[0: state_num: self.n_states], 
+                                self.num_veh, self.N_horz)
+        self.y_opt = ca.reshape(u_opt[1: state_num: self.n_states], 
+                                self.num_veh, self.N_horz)
+        self.v_opt = ca.reshape(u_opt[2: state_num: self.n_states],
+                                self.num_veh, self.N_horz)
+        self.theta_opt = ca.reshape(u_opt[3: state_num: self.n_states],
+                                    self.num_veh, self.N_horz)
+        self.steer_opt = ca.reshape(u_opt[4: state_num: self.n_states], 
+                                    self.num_veh, self.N_horz)
+        self.a_opt = ca.reshape(u_opt[state_num: state_num+action_num: self.n_controls], 
+                                    self.n_controls, self.N_horz-1)
+        self.steerate_opt = ca.reshape(u_opt[state_num+1: state_num+action_num: self.n_controls],
+                                       self.n_controls, self.N_horz-1)
+
+    def generate_object(self, r, q):
+        R = ca.SX(r)
+        Q = ca.SX(q)
+        # objective is to minimize the input effort and 
+        # deviation to reference trajectory in predicted horizon
+        for i in range(1, self.N_horz):
+            st = ca.horzcat(self.X[i][0, :], self.X[i][1, :]).T # TODO: think about later how to cat more vehicles
+            ref_st = self.x0[i]
+            error = st - ref_st # TODO: check dimension during test (10 X 1)
+            con = ca.reshape(self.U[i-1], self.n_controls*self.num_veh, 1)
+
+            self.obj += (con.T@R@con)
+            self.obj += (error.T@Q@error)
+
+    def generate_variable(self):
+        
+        # variables are states
+        for i in range(self.N_horz):
+            for j in range(self.num_veh):    
+                self.variable += [self.X[i][j, :].T]
+                self.lbx += [0, -self.max_y, -self.v_cfg.max_v, -2*pi, -self.v_cfg.max_front_wheel_angle]
+                self.ubx += [self.max_x, self.max_y, self.v_cfg.max_v, 2*pi, self.v_cfg.max_front_wheel_angle]
+            
+        # variables are inputs
+        for i in range(self.N_horz-1):
+            for j in range(self.num_veh):
+                self.variable += [self.U[i][j, :].T]
+                self.lbx += [-self.v_cfg.max_acc, -self.v_cfg.max_steer_rate]
+                self.ubx += [self.v_cfg.max_acc, self.v_cfg.max_steer_rate]       
+        
+        # %%
+        # for i in range(self.N_horz-1):
+        #     self.variable += [ca.reshape(self.U[i], self.num_veh*self.n_controls, 1)]
+        #     lbx_temp = [-self.v_cfg.max_acc, -self.v_cfg.max_steer_rate]*self.num_veh
+        #     ubx_temp = [self.v_cfg.max_acc, self.v_cfg.max_steer_rate]*self.num_veh
+        #     self.lbx += [lbx_temp[row][col] for col in range(self.n_controls) for row in range(self.num_veh)]
+        #     self.ubx += [ubx_temp[row][col] for col in range(self.n_controls) for row in range(self.num_veh)]
+        
+        # %% variables are dual variables
+        for i in range(self.N_horz):
+            for j in range(self.num_veh):
+                self.variable += [self.LAMBDA[i][j, :].T]
+                self.lbx += [0.0, 0.0, 0.0, 0.0]
+                self.ubx += [100000, 100000, 100000, 100000]
+                
+
+    def generate_constrain(self):
+        # constraints for state transition functions
+        
+        # specify the initial position, num_veh X N_horz
+        
+        self.constrains += [ca.reshape(self.X[0].T, (self.num_veh*self.n_states, 1))
+                            -self.init_state]
+        self.lbg += [0, 0, 0, 0, 0]*self.num_veh
+        self.ubg += [0, 0, 0, 0, 0]*self.num_veh
+        
+        # state transition via Euler approximation, num_veh*N_horz X 1
+        for i in range(self.N_horz-1):
+            st = ca.reshape(self.X[i], (self.num_veh*self.n_states, 1))
+            con = ca.reshape(self.U[i], (self.num_veh*self.n_controls, 1))
+            f_value = self.f(st, con) # using ca.Function to represent the intermidiate terms
+            st_next_euler = st+self.dt*f_value
+            st_next = ca.reshape(self.X[i+1], (self.num_veh*self.n_states, 1))
+            self.constrains += [st_next-st_next_euler]
+            self.lbg += [0, 0, 0, 0, 0]*self.num_veh
+            self.ubg += [0, 0, 0, 0, 0]*self.num_veh
+        
+        # constraints for collision avoidance
+        for i_tstep in range(1, self.N_horz): # iterate each predicted time horizon
+            A = []
+            b = []
+            lamb = []
+            # generate polytopic set if vehicles at i_tstep
+            for i_veh in range(self.num_veh):
+                veh_vertices = generate_vehicle_vertices(self.X[i_tstep][i_veh, [0, 1, 3]]) # 4 x 2
+                A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices)
+                A += [A_temp] # 4 X 2
+                b += [b_temp] # 4 X 1
+                lamb += [self.LAMBDA[i_tstep][i_veh, :].T] # 4 X 1
+            # TODO: think about later how to formulate with multiple vehicles
+            # for 2 vehicles: 1 pair
+            self.constrains += [-b[0].T@lamb[0]-b[1].T@lamb[1]] # (4a)
+            self.lbg += [0.001] # minimal distance req.
+            self.ubg += [1000]
+            self.constrains += [A[0].T@lamb[0]+A[1].T@lamb[1]] # (4b, 4c)
+            self.lbg += [0, 0]
+            self.ubg += [0, 0]
+            self.constrains += [ca.mtimes((A[0].T@lamb[0]).T, A[0].T@lamb[0])] # dual norm of \lambda.T@A <= 1
+            self.lbg += [0]
+            self.ubg += [1]
