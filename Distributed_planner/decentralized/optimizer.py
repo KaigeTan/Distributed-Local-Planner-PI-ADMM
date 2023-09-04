50d8fd3e8893cfbe43947cdaee38a952c474a106
Kaige Tan <kaiget@UG.KTH.SE>
2023-09-04 16:11:04 +0200

    update decentralized algorithm

--- Distributed_planner/decentralized/optimizer.py
+++ Distributed_planner/decentralized/optimizer.py
@@ -3,6 +3,7 @@ import casadi as ca
 import numpy as np
 from math import *
 from .util import compute_square_halfspaces_ca, generate_vehicle_vertices
+import copy
 
 class OBCAOptimizer:
     def __init__(self, cfg: VehicleConfig = VehicleConfig()) -> None:
@@ -27,10 +28,12 @@ class OBCAOptimizer:
         self.dt = cfg.dt
         self.ref_traj = cfg.ref_traj_gen()
         self.N_horz = 10 # control horizon
+        self.primal_thres = 1
+        self.dual_thres = 1
+        
 
     # %% below are the functions used at the local vehicle side
-    def local_initialize(self, t_step, init_state, bar_state, i_veh, max_x, max_y):
-        
+    def local_initialize(self, t_step, init_state, bar_state_prev, i_veh, max_x, max_y):
         self.constrains = []
         self.x0 = []
         self.lbg = []
@@ -47,10 +50,9 @@ class OBCAOptimizer:
         self.max_x = max_x
         self.max_y = max_y
         self.veh_idx = i_veh
-        self.bar_state = bar_state
+        self.bar_state = copy.deepcopy(bar_state_prev) # TODO: check isolation
 
     def local_build_model(self) -> bool:
-        
         x = ca.SX.sym('x')
         y = ca.SX.sym('y')
         v = ca.SX.sym('v')
@@ -67,7 +69,6 @@ class OBCAOptimizer:
         self.X = ca.SX.sym('X', self.n_states, self.N_horz) # state and ctrl variable indicates the global optimization
         self.U = ca.SX.sym('U', self.n_controls, self.N_horz-1)
         self.obj = 0
-
         return True
 
     def local_solve(self):
@@ -87,8 +88,12 @@ class OBCAOptimizer:
         self.steer_opt = u_opt[4: state_num: self.n_states]
         self.a_opt = u_opt[state_num: state_num+action_num: self.n_controls]
         self.steerate_opt = u_opt[state_num+1: state_num+action_num: self.n_controls]
+        
+        # update the bar_state
         self.bar_z = ca.horzcat(self.x_opt, self.y_opt, self.theta_opt)
-
+        self.bar_x = ca.horzcat(self.x_opt, self.y_opt, self.v_opt, self.theta_opt, self.steer_opt)
+        
+        
     def local_generate_object(self, r, q):
         R = ca.SX(r)
         Q = ca.SX(q)
@@ -104,7 +109,6 @@ class OBCAOptimizer:
             self.obj += (error.T@Q@error) # deviation to reference trajectory
 
     def local_generate_variable(self):
-        
         # variables are states
         for i in range(self.N_horz):
             self.variable += [self.X[:, i]]
@@ -140,24 +144,33 @@ class OBCAOptimizer:
         # constraints for collision avoidance
         for i_tstep in range(1, self.N_horz): # iterate each predicted time horizon
             # generate polytopic set if vehicles at i_tstep
-            veh_vertices = generate_vehicle_vertices(self.X[:, i_tstep][0, 1, 3]) # 4 x 2
+            veh_vertices = generate_vehicle_vertices(self.X[:, i_tstep][0, 1, 3]) # 4 x 1
             A, b = compute_square_halfspaces_ca(veh_vertices)
             # TODO: think about later how to formulate with multiple vehicles
             # for 2 vehicles: 1 pair
             self.constrains += [-b.T@self.bar_state.lamb[self.veh_idx, i_tstep, :]
                                 -np.dot(self.bar_state.b[1-self.veh_idx, i_tstep, :], 
                                         self.bar_state.lamb[1-self.veh_idx, i_tstep, :])] # (5a)
-            self.lbg += [0.001] # minimal distance req.
+            self.lbg += [0] # minimal distance req.
             self.ubg += [1000]
             self.constrains += [A.T@self.bar_state.lamb[self.veh_idx, i_tstep, :]
                                 +self.bar_state.s[i_tstep, :]] # (5b)
             self.lbg += [0, 0]
             self.ubg += [0, 0]
 
+    def bar_state_update(self, bar_z):
+        # update bar_A and bar_b for all predicted horizons, bar_z contains information for all vehicles
+        for i_tstep in range(self.N_horz): # iterate each predicted time horizon
+            for i_veh in range(self.num_veh):
+                # generate polytopic set if vehicles at i_tstep
+                veh_vertices_bar = generate_vehicle_vertices(np.array(bar_z[i_veh][i_tstep, :]).ravel()) # 4 x 1
+                A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices_bar) 
+                self.bar_state.A[i_veh, i_tstep, :, :] = np.array(A_temp) # TODO: check the horizon, 4 X 2
+                self.bar_state.b[i_veh, i_tstep, :] = np.array(b_temp).ravel() # TODO: check the horizon, 4 X 1
+    
 
     # %% below are functions used at the edge coordinator side
-    def edge_initialize(self, bar_z):
-        
+    def edge_initialize(self, max_x, max_y):
         self.constrains = []
         self.x0 = []
         self.lbg = []
@@ -166,56 +179,72 @@ class OBCAOptimizer:
         self.ubx = []
         self.variable = []
         
-        # get bar_A and bar_b for all predicted horizons
-        self.A = []
-        self.b = []
-        for i_tstep in range(self.N_horz): # iterate each predicted time horizon
-            for i_veh in range(self.num_veh):
-                # generate polytopic set if vehicles at i_tstep
-                veh_vertices = generate_vehicle_vertices(bar_z[i_veh][i_tstep, :]) # 4 x 2
-                A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices) 
-                self.A += [A_temp]
-                self.b += [b_temp]
+        # # get bar_A and bar_b for all predicted horizons
+        # self.A = []
+        # self.b = []
+        # for i_tstep in range(self.N_horz): # iterate each predicted time horizon
+        #     for i_veh in range(self.num_veh):
+        #         # generate polytopic set if vehicles at i_tstep
+        #         veh_vertices = generate_vehicle_vertices(bar_z[i_veh][i_tstep, :]) # 4 x 2
+        #         A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices) 
+        #         self.A += [A_temp]
+        #         self.b += [b_temp]
         
         # TODO: check x0
         for i_t in range(self.N_horz):
-            x0_temp = self.ref_traj[i_veh][t_step+i_t]
-            self.x0 += [x0_temp] # state variable initialization, N_horz X n_state
-        self.x0 += [[0]*(self.n_controls*(self.N_horz-1))] # control variable initialization
+            self.x0 += [[0.01]*2*self.n_dual_variable] # LAMBDA at a single time step, 1 X 2*self.n_dual_variable
+        for i_t in range(self.N_horz):
+            self.x0 += [[0]*self.n_s] # s at a single time step, 1 X self.n_s
         self.max_x = max_x
         self.max_y = max_y
     
     def edge_build_model(self) -> bool:
-        
         self.LAMBDA = ca.SX.sym('LAMBDA', self.num_veh, self.n_dual_variable, self.N_horz)
         self.s = ca.SX.sym('s', self.n_s, self.N_horz)
         self.obj = 0
-
         return True
 
+    def edge_solve(self):
+        nlp_prob = {'f': self.obj, 'x': ca.vertcat(*self.variable),
+                    'g': ca.vertcat(*self.constrains)}
+        solver = ca.nlpsol('solver', 'ipopt', nlp_prob)
+        sol = solver(x0=ca.vertcat(*self.x0), lbx=self.lbx, ubx=self.ubx,
+                     ubg=self.ubg, lbg=self.lbg)
+        u_opt = sol['x']
+        # get optimized result
+        lambda_num = 2*self.n_dual_variable*self.N_horz
+        lambda_bar = np.array(np.round(u_opt[: lambda_num], decimals=4))
+        s_bar = np.array(np.round(u_opt[lambda_num: ], decimals=4))
+        # parse the results, TODO: double check the result
+        # annoying, have to use two steps to split into 3d array
+        lambda_bar = np.reshape(lambda_bar, (self.N_horz, self.num_veh*self.n_dual_variable)) # time-step wise
+        lambda_bar = np.reshape(lambda_bar, (self.N_horz, self.num_veh, self.n_dual_variable), 'F') # N_horz X num_veh X n_dual_variable
+        # update the bar_state for the iteration
+        self.bar_state.lamb = np.transpose(lambda_bar, (1, 0, 2))
+        self.bar_state.s = np.reshape(s_bar, (10, 2))
+    
     def edge_generate_constrain(self):
         # constraints for (6a) and (6b)
         for i_tstep in range(self.N_horz):
-            self.constrains += self.A[self.N_horz*2].T@self.LAMBDA[i_tstep][0, :] + self.s[:, i_tstep] # 2 x 1
+            self.constrains += [self.bar_state.A[0, i_tstep].T@self.LAMBDA[i_tstep][0, :].T + self.s[:, i_tstep]] # 2 x 1
             self.lbg += [0, 0]
             self.ubg += [0, 0]
-            self.constrains += self.A[1+self.N_horz*2].T@self.LAMBDA[i_tstep][1, :] - self.s[:, i_tstep]
+            self.constrains += [self.bar_state.A[1, i_tstep].T@self.LAMBDA[i_tstep][0, :].T - self.s[:, i_tstep]]
             self.lbg += [0, 0]
             self.ubg += [0, 0]
-            self.constrains += [-self.b[self.N_horz*2].T@self.LAMBDA[i_tstep][0, :]
-                                -self.b[1+self.N_horz*2].T@self.LAMBDA[i_tstep][1, :]] # (6c-1)
-            self.lbg += [0.001] # minimal distance req.
+            self.constrains += [-(self.bar_state.b[0, i_tstep][np.newaxis, :])@self.LAMBDA[i_tstep][0, :].T
+                                -(self.bar_state.b[1, i_tstep][np.newaxis, :])@self.LAMBDA[i_tstep][1, :].T] # (6c-1)
+            self.lbg += [1] # minimal distance req.
             self.ubg += [1000]
-            self.constrains += [s[:, 0].T@s[:, 0]]
+            self.constrains += [self.s[:, i_tstep].T@self.s[:, i_tstep]]
             self.lbg += [0]
             self.ubg += [1]
             
-    def edge_generate_variable(self):
-        
+    def edge_generate_variable(self):        
         # variables are lambda
         for i in range(self.N_horz):
             self.variable += [ca.reshape(self.LAMBDA[i], 2*self.n_dual_variable, 1)] # 8 x 1
-            self.lbx += [0]*2*self.n_dual_variable
+            self.lbx += [0.0001]*2*self.n_dual_variable
             self.ubx += [1000]*2*self.n_dual_variable
             
         # variables are s
@@ -223,8 +252,16 @@ class OBCAOptimizer:
             self.variable += [self.s[:, i]]
             self.lbx += [-1000000]*self.n_s
             self.ubx += [1000000]*self.n_s
-
-
+    
+    def edge_generate_object(self):
+        # objective is to minimize the lower-level CA problem
+        # TODO: check the logic here, objective function in (6) may has problem (no sum of time steps)!!!
+        for i in range(self.N_horz):
+            # the original formulation in (6) is max of minus xxx, here we reverse the signs
+            self.obj += (self.bar_state.b[0, i][np.newaxis, :])@(self.LAMBDA[i][0, :].T) + \
+                        (self.bar_state.b[1, i][np.newaxis, :])@(self.LAMBDA[i][1, :].T)
+            # note only LAMBDA is included in the obj function, s is not used
+            
     # %% define the initialized bar_state
     def create_bar_state(self):
         bar_state = self.mid_state(self)
@@ -233,5 +270,6 @@ class OBCAOptimizer:
     class mid_state:
         def __init__(self, outer):
             self.lamb = np.zeros((outer.num_veh, outer.N_horz, outer.n_dual_variable)) # 2x10x4
+            self.A = np.zeros((outer.num_veh, outer.N_horz, outer.n_dual_variable, 2)) # 2x10x4X2
             self.b = np.zeros((outer.num_veh, outer.N_horz, outer.n_dual_variable)) # 2x10x4
             self.s = np.zeros((outer.N_horz, outer.n_s)) # 10x2
--- Distributed_planner/decentralized_overtaking.py
+++ Distributed_planner/decentralized_overtaking.py
@@ -5,9 +5,11 @@ from pypoman import plot_polygon
 import sys
 sys.path.append('../')
 import decentralized
+import copy
 
 
 state_record = []
+iter_num_record = []
 # %% obca optimization
 optimizer = decentralized.OBCAOptimizer()
 init_state = [arr[0, :] for arr in optimizer.ref_traj]
@@ -16,42 +18,60 @@ state_record = [init_state]
 init_state = np.reshape(init_state, [optimizer.num_veh*optimizer.n_states, 1]) # 10 X 1
 
 for t_step in range(int(optimizer.T/optimizer.dt - optimizer.N_horz)): # TODO: check if -1
-    bar_state = optimizer.create_bar_state()
-    # optimize from vehicle side
-    bar_z = []
-    for i_veh in range(optimizer.num_veh):
-        optimizer.local_initialize(t_step, init_state[i_veh*optimizer.n_states: (i_veh+1)*optimizer.n_states], 
-                                   bar_state, i_veh, max_x=150, max_y=20)
-        optimizer.local_build_model()
-        optimizer.local_generate_constrain()
-        optimizer.local_generate_variable()
-        r = 0.1*np.eye(optimizer.n_controls)
-        q = np.eye(optimizer.n_states)
-        optimizer.local_generate_object(r, q)
-        optimizer.local_solve()
-        bar_z += [np.array(optimizer.bar_z)]
-    # optimize from RSU side
-    optimizer.edge_initialize(bar_z)
-    optimizer.edge_build_model()
-    optimizer.edge_generate_constrain()
-    optimizer.edge_generate_variable()
+    if t_step == 16:
+        print('goes here!')
+    # initialize at each time step, bar_state: lambda_bar, b_bar, s_bar; bar_ctrl: a_opt, steerate_opt
+    bar_state_prev = optimizer.create_bar_state()
+    bar_ctrl_prev = np.array([[0]*(optimizer.N_horz-1)*optimizer.n_controls]*optimizer.num_veh)
+    i_iter = 0
+    while True:
+        i_iter += 1
+        # optimize from vehicle side
+        bar_z = []
+        bar_x = []
+        bar_ctrl = []
+        for i_veh in range(optimizer.num_veh):
+            optimizer.local_initialize(t_step, init_state[i_veh*optimizer.n_states: (i_veh+1)*optimizer.n_states], 
+                                       bar_state_prev, i_veh, max_x=150, max_y=20)
+            optimizer.local_build_model()
+            optimizer.local_generate_constrain()
+            optimizer.local_generate_variable()
+            r = 0.1*np.eye(optimizer.n_controls)
+            q = np.eye(optimizer.n_states)
+            optimizer.local_generate_object(r, q)
+            optimizer.local_solve()
+            bar_z += [np.array(optimizer.bar_z)]
+            bar_x += [np.array(optimizer.bar_x)]
+            bar_ctrl += [np.append(np.array(optimizer.a_opt).T, np.array(optimizer.steerate_opt).T)]
         
-    # check the optimization results
-    x_opt = np.array(np.round(optimizer.x_opt, decimals=3))
-    y_opt = np.array(np.round(optimizer.y_opt, decimals=3))
-    v_opt = np.array(np.round(optimizer.v_opt, decimals=3))
-    heading_opt = np.array(np.round(optimizer.theta_opt, decimals=3))
-    steer_opt = np.array(np.round(optimizer.steer_opt, decimals=3))
-    a_opt = np.array(np.round(optimizer.a_opt, decimals=3))
-    steerate_opt = np.array(np.round(optimizer.steerate_opt, decimals=3))
+        # information exchange, bar_A & bar_b update
+        optimizer.bar_state_update(bar_z)
         
+        # optimize from RSU side
+        optimizer.edge_initialize(max_x=150, max_y=20)
+        optimizer.edge_build_model()
+        optimizer.edge_generate_constrain()
+        optimizer.edge_generate_variable()
+        optimizer.edge_generate_object()
+        optimizer.edge_solve()
+        # check residuals
+        primal_res = np.sum(np.sqrt((bar_ctrl[0]-bar_ctrl_prev[0])*(bar_ctrl[0]-bar_ctrl_prev[0])) + \
+                            np.sqrt((bar_ctrl[1]-bar_ctrl_prev[1])*(bar_ctrl[1]-bar_ctrl_prev[1])))
+        dual_res = np.sum(np.sqrt((optimizer.bar_state.lamb-bar_state_prev.lamb)*(optimizer.bar_state.lamb-bar_state_prev.lamb)))
+        if (primal_res <= optimizer.primal_thres and dual_res <= optimizer.dual_thres) or i_iter > 5:
+            if i_iter > 5:
+                print('goes here')
+            break # primal and dual residual within threshold, converge
+        else:    
+            # update bar_state in the current iteration
+            bar_state_prev = copy.deepcopy(optimizer.bar_state)
+            # update previous iteration result
+            bar_ctrl_prev = copy.deepcopy(bar_ctrl)
+    
     # iterate to the next time step
-    init_state = np.concatenate((x_opt[:, 1].reshape((-1, 1)), 
-                                 y_opt[:, 1].reshape((-1, 1)), 
-                                 v_opt[:, 1].reshape((-1, 1)),
-                                 heading_opt[:, 1].reshape((-1, 1)),
-                                 steer_opt[:, 1].reshape((-1, 1))), axis=1)
+    init_state = np.vstack((bar_x[0][1], bar_x[1][1]))
     state_record += [init_state]
+    iter_num_record += [i_iter]
     init_state = np.reshape(init_state, [optimizer.num_veh*optimizer.n_states, 1]) # 10 X 1
     
 # visualization
@@ -68,33 +88,11 @@ ax.set_xlim(0,90)
 
 # plot the vehicle polygons
 for i in range(len(state_record)):
-    v1_verts = centralized.generate_vehicle_vertices(state_record[i][0, [0, 1, 3]])
+    v1_verts = decentralized.generate_vehicle_vertices(state_record[i][0, [0, 1, 3]])
     plot_polygon(v1_verts, fill=False, color='b')
-    v2_verts = centralized.generate_vehicle_vertices(state_record[i][1, [0, 1, 3]])
+    v2_verts = decentralized.generate_vehicle_vertices(state_record[i][1, [0, 1, 3]])
     plot_polygon(v2_verts, fill=False, color='r')
 
 plt.show()
 
-# plot_polygon(parking_obs1)
-# plot_polygon(parking_obs2, color='r')
-# ax.legend()
-# result_state = np.array([x_opt, y_opt, heading_opt]).T
-# for state in result_state:
-#     verts = pyobca.generate_vehicle_vertices(
-#         pyobca.SE2State(state[0], state[1], state[2]), base_link=True)
-#     plot_polygon(verts, fill=False, color='b')
-# v_opt = optimizer.v_opt.elements()
-# a_opt = optimizer.a_opt.elements()
-# t = [optimizer.T*k for k in range(len(v_opt))]
-# t_a = [optimizer.T*k for k in range(len(a_opt))]
-
-
-# fig2, ax2 = plt.subplots(3)
-# ax2[0].plot(t, v_opt, label='v-t')
-# ax2[1].plot(t_a, a_opt, label='a-t')
-# ax2[2].plot(t, steer_opt, label='steering-t')
-# ax2[0].legend()
-# ax2[1].legend()
-# ax2[2].legend()
-# plt.show()
 
