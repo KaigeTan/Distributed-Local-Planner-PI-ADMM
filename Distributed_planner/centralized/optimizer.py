a596e09adf770c491a2e7feaca2b7e3eb8d4a213
Kaige Tan <kaiget@UG.KTH.SE>
2023-11-20 15:33:57 +0100

    add the communication delay in central solution

--- Distributed_planner/centralized/optimizer.py
+++ Distributed_planner/centralized/optimizer.py
@@ -2,7 +2,8 @@ from .veh_config import VehicleConfig
 import casadi as ca
 import numpy as np
 from math import *
-from .util import compute_square_halfspaces_ca, generate_vehicle_vertices
+from .util import compute_square_halfspaces_ca, generate_vehicle_vertices, \
+    compute_square_halfspaces_ca_rot, compute_square_halfspaces_ca_prob
 
 class OBCAOptimizer:
     def __init__(self, cfg: VehicleConfig = VehicleConfig()) -> None:
@@ -25,10 +26,9 @@ class OBCAOptimizer:
         self.T = cfg.T
         self.dt = cfg.dt
         self.ref_traj = cfg.ref_traj_gen()
-        self.N_horz = 10 # control horizon
+        self.N_horz = 15 # control horizon
 
-    def initialize(self, t_step, init_state, max_x, max_y):
-        
+    def initialize(self, t_step, init_state, max_x, max_y, prob, min_dis):
         self.constrains = []
         self.x0 = []
         self.lbg = []
@@ -44,12 +44,14 @@ class OBCAOptimizer:
                 x0_temp = np.hstack([x0_temp, self.ref_traj[i_veh][t_step+i_t]])
             self.x0 += [x0_temp] # state variable initialization, N_horz X num_veh*n_state
         self.x0 += [[0]*(self.num_veh*self.n_controls*(self.N_horz-1))] # control variable initialization
-        self.x0 += [[0.1]*(self.num_veh*self.n_dual_variable*self.N_horz)] # TODO: check dual variable number, dual variable initialization
+        self.x0 += [[0.1]*(self.num_veh*self.n_dual_variable*(self.N_horz-1))] # TODO: check dual variable number, dual variable initialization
         self.max_x = max_x
         self.max_y = max_y
+        self.prob = prob
+        self.min_dis = min_dis
 
     def build_model(self) -> bool:
-        
+        # state function is based on 'a distributed multi-robot coordination ...' eq.(7)-(8)
         x = ca.SX.sym('x', self.num_veh)
         y = ca.SX.sym('y', self.num_veh)
         v = ca.SX.sym('v', self.num_veh)
@@ -60,14 +62,15 @@ class OBCAOptimizer:
         self.state = ca.vertcat(ca.vertcat(x, y, v, theta), steering) # 5*num_veh X 1
         self.control = ca.vertcat(a, steering_rate) # 2*num_veh X 1
         beta = ca.atan(self.lr*ca.tan(steering)/(self.lr+self.lf))
+        # rhs is delta_state/delta_t
         self.rhs = ca.vertcat(ca.vertcat(v*ca.cos(theta+beta), v*ca.sin(theta+beta),
-                                   a, v/self.lr*ca.sin(beta)), steering_rate) # 5*num_veh X 1, TODO: doublecheck the last-but-two term
+                                    a, v/(self.lr+self.lf)*ca.cos(beta)*ca.tan(steering)), steering_rate) # TODO: doublecheck the last-but-two term， 5*num_veh X 1 
+#                                   a, v/self.lr*ca.sin(beta)), steering_rate)
         self.f = ca.Function('f', [self.state, self.control], [self.rhs])
         self.X = ca.SX.sym('X', self.num_veh, self.n_states, self.N_horz) # state and ctrl variable indicates the global optimization
         self.U = ca.SX.sym('U', self.num_veh, self.n_controls, self.N_horz-1)
-        self.LAMBDA = ca.SX.sym('LAMBDA', self.num_veh, self.n_dual_variable, self.N_horz)
+        self.LAMBDA = ca.SX.sym('LAMBDA', self.num_veh, self.n_dual_variable, self.N_horz-1)
         self.obj = 0
-
         return True
 
     def solve(self):
@@ -94,6 +97,7 @@ class OBCAOptimizer:
                                     self.n_controls, self.N_horz-1)
         self.steerate_opt = ca.reshape(u_opt[state_num+1: state_num+action_num: self.n_controls],
                                        self.n_controls, self.N_horz-1)
+        self.lambda_result = u_opt[state_num+action_num: ]
 
     def generate_object(self, r, q):
         R = ca.SX(r)
@@ -105,12 +109,10 @@ class OBCAOptimizer:
             ref_st = self.x0[i]
             error = st - ref_st # TODO: check dimension during test (10 X 1)
             con = ca.reshape(self.U[i-1], self.n_controls*self.num_veh, 1)
-
             self.obj += (con.T@R@con)
             self.obj += (error.T@Q@error)
 
     def generate_variable(self):
-        
         # variables are states
         for i in range(self.N_horz):
             for j in range(self.num_veh):    
@@ -134,7 +136,7 @@ class OBCAOptimizer:
         #     self.ubx += [ubx_temp[row][col] for col in range(self.n_controls) for row in range(self.num_veh)]
         
         # %% variables are dual variables
-        for i in range(self.N_horz):
+        for i in range(self.N_horz-1):
             for j in range(self.num_veh):
                 self.variable += [self.LAMBDA[i][j, :].T]
                 self.lbx += [0.0, 0.0, 0.0, 0.0]
@@ -143,9 +145,7 @@ class OBCAOptimizer:
 
     def generate_constrain(self):
         # constraints for state transition functions
-        
         # specify the initial position, num_veh X N_horz
-        
         self.constrains += [ca.reshape(self.X[0].T, (self.num_veh*self.n_states, 1))
                             -self.init_state]
         self.lbg += [0, 0, 0, 0, 0]*self.num_veh
@@ -169,15 +169,20 @@ class OBCAOptimizer:
             lamb = []
             # generate polytopic set if vehicles at i_tstep
             for i_veh in range(self.num_veh):
-                veh_vertices = generate_vehicle_vertices(self.X[i_tstep][i_veh, [0, 1, 3]]) # 4 x 2
-                A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices)
+                # if consider the communication delay, calcuate the halfspace expression by rotational &
+                # translational matrix instead of the edge points
+                if self.prob:
+                    A_temp, b_temp = compute_square_halfspaces_ca_prob(self.X[i_tstep][i_veh, :])
+                else:
+                    veh_vertices = generate_vehicle_vertices(self.X[i_tstep][i_veh, :]) # 4 x 2
+                    A_temp, b_temp = compute_square_halfspaces_ca(veh_vertices)
                 A += [A_temp] # 4 X 2
                 b += [b_temp] # 4 X 1
-                lamb += [self.LAMBDA[i_tstep][i_veh, :].T] # 4 X 1
+                lamb += [self.LAMBDA[i_tstep-1][i_veh, :].T] # 4 X 1
             # TODO: think about later how to formulate with multiple vehicles
             # for 2 vehicles: 1 pair
             self.constrains += [-b[0].T@lamb[0]-b[1].T@lamb[1]] # (4a)
-            self.lbg += [3] # minimal distance req.
+            self.lbg += [self.min_dis] # minimal distance req.
             self.ubg += [1000]
             self.constrains += [A[0].T@lamb[0]+A[1].T@lamb[1]] # (4b, 4c)
             self.lbg += [0, 0]
